1e078fbd6c9c758603a7d2d316821187b0aacf28
CAI <643367306@qq.com>
2020-05-31 22:08:50 +0800

    update file

--- git_probe.py
+++ git_probe.py
@@ -1,10 +1,29 @@
 # coding:utf-8
 
 
-def excel_task():
-	pass
+class ExcelUtil(object):
+
+	def __int__(self):
+		pass
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		if all([exc_type, exc_val, exc_tb]):
+			print('error')
+		return True
+
+	@staticmethod
+	def read_content(dir_path):
+		with open(dir_path, 'r') as f:
+			return f.read()
+
+	def task(self):
+		pass
 
 
 if __name__ == '__main__':
-	excel_task()
+	with ExcelUtil() as eu:
+		eu.task()
 
