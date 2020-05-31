895305087360b6b086b7999939bc8cba22e05fd0
CAI <643367306@qq.com>
2020-05-31 22:00:24 +0800

    update file

--- git_probe.py
+++ git_probe.py
@@ -1,23 +1,28 @@
 # coding:utf-8
 
 
-class ExcelUtil(object):
+# class ExcelUtil(object):
+#
+# 	def __int__(self):
+# 		pass
+#
+# 	def __enter__(self):
+# 		return self
+#
+# 	def __exit__(self, exc_type, exc_val, exc_tb):
+# 		if all([exc_type, exc_val, exc_tb]):
+# 			print('error')
+# 		return True
+#
+# 	def task(self):
+# 		pass
 
-	def __int__(self):
-		pass
 
-	def __enter__(self):
-		return self
-
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		if all([exc_type, exc_val, exc_tb]):
-			print('error')
-		return True
-
-	def task(self):
-		pass
+def task():
+	pass
 
 
 if __name__ == '__main__':
-	with ExcelUtil() as eu:
-		eu.task()
+	# with ExcelUtil() as eu:
+	# 	eu.task()
+	task()
